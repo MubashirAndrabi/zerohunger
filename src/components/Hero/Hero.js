5f4d3dbd07511e90477e39fd9eb3c388c94edb94
tmouhamed <s.toqaa@gmail.com>
2020-08-12 13:22:01 -0400

    add info box to the hero section and finished it

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,9 +10,9 @@ function Hero() {
     <div className="hero">
         <Row className="h-100 align-items-center">
           <Col md={{ span: 6, offset: 3 }} className="text-center">
-            <h1 className="font-weight-light">dummy text of the printing</h1>
-            <p>remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets </p>
-            <Button>Read me</Button>
+            <h1 className="hero-title font-weight-bolder">dummy text of the printing</h1>
+            <p className="hero-subtitle">remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets </p>
+            <Button size="lg">Read me</Button>
           </Col>
         </Row>
     </div>
