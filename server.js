ebdafd703d1b9846e39c8ded25cd34ce2b91cb3a
tmouhamed <s.toqaa@gmail.com>
2020-08-27 14:49:03 -0400

    built an API server with JWT authentcation

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+const errorHandler = require('_helpers/errorHandler.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
